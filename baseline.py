28b2791fa225bac63dabbd29933caac8fe4fde68
Sabin <50198431+sabin5105@users.noreply.github.com>
2022-09-28 13:45:46 +0900

    Create baseline.py for reusablility
    Just put pasted code from Decision_Tree.ipynb to use again the same code pretty

---
+++ baseline.py
@@ -0,0 +1,53 @@
+import warnings
+warnings.filterwarnings('ignore')
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from sklearn.metrics import *
+from sklearn.tree import DecisionTreeClassifier
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+batch_size = 4
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                          shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                         shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize because, By default, imshow normalizes the data to its min and max.
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+def main():
+    print(trainset)
+    print("-"*70)
+    print(testset)
+        
+    dataiter = iter(trainloader)
+    images, labels = dataiter.next()
+
+    imshow(torchvision.utils.make_grid(images))
+    print(' '.join('%5s' % classes[labels[j]] for j in range(batch_size)))
+
+
+if __name__ == '__main__':
+    main()
+    
