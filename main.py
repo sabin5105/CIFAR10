ae0830480a3ba31d71df7a1b24de06003670c723
Sabin <sabin5105@gmail.com>
2022-11-18 18:52:02 +0900

    update parameters, comment
    parameters:
     - num_workers = 2 ->16
    
    comment:
     - RandomCrop paper
     - RandomHorizontalFlip paper
     - normalize parameter

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -27,10 +28,11 @@ start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 # Data
 print('==> Preparing data..')
 transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
+    transforms.RandomCrop(32, padding=4), # Randomly crop the image to 32x32 -> ryo takahashi, Data Augmentation using Random Image Cropping and Patching for Deep CNNs, 2019
+    transforms.RandomHorizontalFlip(), # Randomly flip the data horizontally -> Andrew G Howard, MobileNets: Efficient Convolutional Neural Networks for Mobile Vision Applications, 2017
     transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)), # # best practice for normalization
 ])
 
 transform_test = transforms.Compose([
@@ -41,12 +43,12 @@ transform_test = transforms.Compose([
 trainset = torchvision.datasets.CIFAR10(
     root='./data', train=True, download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=128, shuffle=True, num_workers=2)
+    trainset, batch_size=128, shuffle=True, num_workers=16)
 
 testset = torchvision.datasets.CIFAR10(
     root='./data', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(
-    testset, batch_size=100, shuffle=False, num_workers=2)
+    testset, batch_size=100, shuffle=False, num_workers=16)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer',
            'dog', 'frog', 'horse', 'ship', 'truck')
@@ -136,8 +138,12 @@ def test(epoch):
         torch.save(state, './checkpoint/ckpt.pth')
         best_acc = acc
 
+def main():
+    torch.multiprocessing.freeze_support()
+    for epoch in range(start_epoch, start_epoch+200):
+        train(epoch)
+        test(epoch)
+        scheduler.step()
 
-for epoch in range(start_epoch, start_epoch+200):
-    train(epoch)
-    test(epoch)
-    scheduler.step()
+if __name__=="__main__":
+    main()
